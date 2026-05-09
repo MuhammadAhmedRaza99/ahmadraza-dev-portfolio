b65616cec6da331943b108c85682b547b656460a
Ahmed Raza <mahmedraza574@gmail.com>
2026-05-09 23:42:40 +0500

    Update Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Send, GitPullRequest, User, Mail, MessageSquare, ArrowRight } from 'lucide-react';
+import {Send,GitPullRequest, User, Mail, MessageSquare, ArrowRight } from 'lucide-react';
 import { portfolioData } from '../data/portfolioData';
 
 const Contact = () => {
